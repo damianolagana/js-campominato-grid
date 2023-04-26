4fab66cd3665aa0ad26705a336200c4aaacb5a4e
damianolagana <damianolagana@gmail.com>
2023-04-26 17:38:32 +0200

    add click event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log("ciao")
+// selezionare il bottone nella pagina html
+const btnGenerate = document.getElementById("btnPlay")
+const gridBox = document.getElementById("grid")
+
+// quando l'utente clicca accade un evento
+btnGenerate.addEventListener("click",function(){
+    generateGrid();
+})
+
+
+
+
+
+//////////// FUNZIONI ////
+
+function generateGrid(){
+
+
+    for(let i = 1 ; i <= 100 ; i++){
+        const numero = i
+        const newElement = document.createElement("div");
+        newElement.classList.add("square");
+        newElement.innerText = numero;
+
+        gridBox.append(newElement);
+    }
+
+}
+
+
+
+
